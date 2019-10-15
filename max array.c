336c2502271c9423380113cf14b5257a558b4cda
91959 <ayushsatyam146@gmail.com>
2019-10-15 22:22:37 +0530

    max element of an array

---
+++ max array.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int largest(int arr[], int n)
+{
+    int i;
+    int max = arr[0];
+    for (i = 1; i < n; i++)
+        if (arr[i] > max)
+            max = arr[i];
+
+    return max;
+}
