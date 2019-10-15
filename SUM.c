058592fa071ab5e0ffcec03499d3418f46e55448
SatyamKumarSunderam <satyamkumar99386@gmail.com>
2019-10-15 19:32:29 +0530

    update code

---
+++ SUM.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int n1,n2,sum;
+printf("Enter first number : ");
+scanf("%d",&n1);
+printf("Enter second number : ");
+scanf("%d",&n2);
+sum=n1+n2;
+printf("The sum of two numbers is : %d",sum);
+}
