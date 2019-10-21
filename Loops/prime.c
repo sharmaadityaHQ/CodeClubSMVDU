337fe65dec3b0902cf66829b8b2550ac051956ec
maddurisrivalli123 <56594713+maddurisrivalli123@users.noreply.github.com>
2019-10-22 00:51:09 +0530

    Update prime.c

--- Loops/prime.c
+++ Loops/prime.c
@@ -1,19 +1,37 @@
-//Find if no is prime or not
 #include<stdio.h>
 int main(){
     int f=0,i,num;
-    printf("Enter number\n");
+    printf("Enter a whole number\n");
     scanf("%d",&num);
-    for(i=2;i<=num/2;i++){
-        if(num%i==0){
-            f=1;
+    if(num==0)
+    {
+       printf("Zero is neither prime nor composite\n");
+    }
+   else if(num==1)
+    {
+       printf("One is neither prime nor composite\n");
+    }
+   else if(num==2)
+    {
+        printf("The entered number is a prime number ");
+    }
+    else if(num==3)
+    {
+        printf("The entered number is a prime number");
+    }
+    else
+    {
+        for(i=2;i<=num/2;i++){
+        if(num%i==0)
+        {
+            printf("The entered number is a composite number");
+            break;
+        }
+        else
+        {
+            printf("The entered number is a prime number");
             break;
         }
     }
-    if(f==0){
-        printf("Prime no");
-    }
-    else{
-        printf("Not prime");
     }
 }
