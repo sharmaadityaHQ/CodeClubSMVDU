38b2a099ba63d86dcb973d907dae74ff987c9374
maddurisrivalli123 <56594713+maddurisrivalli123@users.noreply.github.com>
2019-10-21 22:35:37 +0530

    Update factorial.c

--- Loops/factorial.c
+++ Loops/factorial.c
@@ -1,11 +1,25 @@
-// Print factorial of a no
+/* Print factorial of a no*/
 #include<stdio.h>
+int factorial(int p);
 int main(){
-    int num,i,res=1;
-    printf("Enter number\n");
+    int num,i,result=1,p,product=1;
+    printf("Enter a whole no.");
     scanf("%d",&num);
-    for(i=1;i<=num;i++){
-        res*=i;
+    if(num>0)
+    {for(i=1;i<=num;i=i+1){
+        result=factorial(i);
+        product=result*product;
+
     }
-    printf("Factorial of %d = %d",num,res);
+    printf("Factorial of %d = %d",num,product);
+    }
+    else if(num==0)
+    printf("the factorial of zero is one");
+
 }
+  int factorial(int p)
+  {
+      int result=1;
+      result=result*p;
+      return result;
+  }
