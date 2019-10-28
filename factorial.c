6fca1a63bcdb7f9cc366c940341caf5fda71604e
urvashimaha <56594705+urvashimaha@users.noreply.github.com>
2019-10-28 22:37:07 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-int n,i,fact=1;
-printf("enter any value of n");
-scanf("%d",&n);
-for(i=1;i<=n;i++)
+int a,i,factorial=1;
+printf("enter any value of a");
+scanf("%d",&a);
+for(i=1;i<=a;i++)
 {
 fact=fact*i;
-printf("the value of fact is %d\n",fact);
+printf("the value of factorial is %d\n",fact);
 }
 return 0;
 }
