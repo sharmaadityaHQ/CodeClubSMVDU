ce15f20cbd0d60718211bda1aa2d1c106b9bf90f
urvashimaha <56594705+urvashimaha@users.noreply.github.com>
2019-10-28 21:57:46 +0530

    Add files via upload

---
+++ prime number.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{int num,i;
+printf("enter a number");
+scanf("%d",&num);
+i=2;
+while(i<=num-1)
+{
+    if(num%i==0)
+{
+printf("not a prime number\n");
+break;
+}
+i++;
+}
+if(i==num)
+printf("prime number\n");
+}
